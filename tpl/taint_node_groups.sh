93d45d01d323cd530fc4a8665b39d56ed15bbb1a
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-04-19 21:30:50 -0500

    [ncheneweth] scripts & tpl refactor, testing AMI update Taint ENV var

--- tpl/taint_node_groups.sh
+++ tpl/taint_node_groups.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 export NODE_GROUP=$1
+export TERRAFORM_STEP=$2
 
 if [[ -v $TAINT ]]; then
-  echo "TAINT is set"
-else
-  echo "TAINT is not set"
-fi
+  case $TERRAFORM_STEP in
+    Plan)
+      echo -n "node_group ${NODE_GROUP} scheduled for taint"
+      ;;
 
-# if [[ -v $TAINT ]]; then
-#   terraform taint "module.eks.module.node_groups.random_pet.node_groups[\"${NODE_GROUP}\"]"
-#   terraform taint "module.eks.module.node_groups.aws_eks_node_group.workers[\"${NODE_GROUP}\"]"
-# fi
+    Apply)
+      echo -n "Apply taint to node_group ${NODE_GROUP}"
+      terraform taint "module.eks.module.node_groups.random_pet.node_groups[\"${NODE_GROUP}\"]"
+      terraform taint "module.eks.module.node_groups.aws_eks_node_group.workers[\"${NODE_GROUP}\"]"
+      ;;
+
+    *)
+      echo -n "error: unknown terraform step (${TERRAFORM_STEP}) provided to taint_node_groups.sh"
+      exit 1
+      ;;
+  esac
+fi
 
 # use circleci api to clear TAINT
