759166c01efaed5d88f00356e9f581ddfc9672e9
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-02-12 19:59:21 -0600

    [ncheneweth] add dashboard and monitors for -eks-base services
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- data.tf
+++ data.tf
@@ -12,25 +12,10 @@ data "aws_subnet_ids" "private" {
   }
 }
 
-data "aws_subnets" "private" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.cluster_vpc.id]
-  }
-
-  tags = {
-    Tier = "Private"
-  }
-}
-
-output "subnet_ids" {
-  value = data.aws_subnets.private.ids
-}
-
-data "aws_subnet" "private_subnets" {
-  for_each = data.aws_subnet_ids.private.ids
-  id       = each.value
-}
+// data "aws_subnet" "private_subnets" {
+//   for_each = data.aws_subnet_ids.private.ids
+//   id       = each.value
+// }
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
