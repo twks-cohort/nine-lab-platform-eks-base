9c797ba1c14888953f57ecf91622cb83ea1e8b1a
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-06-15 10:07:10 -0500

    [ncheneweth] use op orb instead of op run
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/deploy_dashboard.sh
+++ scripts/deploy_dashboard.sh
@@ -24,7 +24,7 @@ export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAcces
 export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
 
 # current versions table
-export TABLE="| dependency | sandbox |   prod   |\\\\n|----|----|----|\\\\n"
+export TABLE="| dependency | -us-east-2 | prod-us-east-1 |\\\\n|----|----|----|\\\\n"
 export EKS_VERSIONS="| eks |"
 export AMI_VERSIONS="| ami |"
 export COREDNS_VERSIONS="| coredns |"
@@ -32,7 +32,7 @@ export KUBE_PROXY_VERSIONS="| kube-proxy |"
 export VPC_CNI_VERSIONS="| vpc-cni |"
 export EBS_CSI_VERSIONS="| ebs-csi |"
 
-declare -a clusters=(sandbox prod)
+declare -a clusters=(sandbox-us-east-2 prod-us-east-1)
 
 echo "generate markdown table with the desired versions of the services managed by the lab-platform-eks-base pipeline for all clusters"
 for cluster in "${clusters[@]}";
